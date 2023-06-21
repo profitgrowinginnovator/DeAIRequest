86d18cbf5731b3287d3e2d54996fe5df4b89db67
maarten ectors <maarten@profitgrowinginnovator.com>
2023-06-21 09:04:51 +0100

    removed svg include to fix the import issue

--- DeAIRequest_0011ai/bacalhauconnector/bacalhauconnector/cli/__init__.py
+++
@@ -1,4 +0,0 @@
-from .version import version
-from .verify import verify
-from .init import init
-from .run import run
--- DeAIRequest_0011ai/bacalhauconnector/bacalhauconnector/sdk/__init__.py
+++
@@ -1,3 +0,0 @@
-from .conda_env import CondaEnv, CondaEnvValidator
-from .importer import import_packages
-from .dataset import dataset
